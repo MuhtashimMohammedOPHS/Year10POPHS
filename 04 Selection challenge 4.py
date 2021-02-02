09c4aeb5c0c2935e346e365ac3149af788121297
MuhtashimMohammedOPHS <71763610+MuhtashimMohammedOPHS@users.noreply.github.com>
2021-02-02 11:31:31 +0000

    Delete 04 Selection challenge 4.py

--- 04 Selection challenge 4.py
+++
@@ -1,8 +0,0 @@
-colour = input("Enter your favourite colour >>>")
-
-if(colour=="red") or (colour=="RED") or (colour=="Red"):
-    print("I like red too")
-
-else:
-    print("I dont like",colour,",",colour,"is for losers")
-
